1fae52e038a2f09987dab5b9e3156713729a5a45
Serenna <serenna.ferrari@hotmail.com>
2024-06-12 20:50:42 -0300

    ADD readme and comments in code

---
+++ src/clirc/probl1/README.md
@@ -0,0 +1,46 @@
+# Tarefa 2 -- Teoria da Computação -- 2021/2
+**Definindo Computação & Açucar Sintático & Código como Dados, Dados como Código**
+**Autores:** Mauricio Uhlig & Serenna Ferrari
+
+## Problema 1
+O primeiro problema da lista é dividido em 3 partes, cada uma com um objetivo diferente.
+
+A parte I pede a construção de uma função no dialeto AON-CLIRC que compare 2 números binários de 3 bits cada, retornando 1 (verdadeiro) quando o número
+da esquerda for maior que o da direita, e 0 em qualquer outro caso.
+
+A parte II pede uma prova que existe uma constante para descobrir o número máximo de portas lógicas para computar a função de maior (mesmas regras 
+discutidas na parte I) no dialeto AON-CLIRC.
+
+Por fim, a parte III requere a mesma prova pedida na parte II, porém para o dialeto NAND-CLIRC.
+
+**Solução - Parte I **
+A solução para a função pedida foi resolvida com o código para a prova requerida pela parte II.
+A função criada para a parte II verifica se o número binário da esquerda é maior que o binário da direita para N bits,
+como consequência, o mesmo código consegue verificar para números de 3 bits. Mais detalhes da implementação logo abaixo.
+
+
+**Solução - Parte II **
+Para essa solução, o ponto inicial foi desenhar um circuito booleano e adicionar
+as comparações bit a bit para identificar o padrão. Com esse número observado (no caso
+do AON, a constante é 5), o código foi criado de forma a checar o caso inicial 
+(comparação de números de 1 bit) e a partir ele, repetir a estrutura de adicionar 5 portas
+lógicas para formar uma lista no dialeto AON-CLIRC, a qual é retornada.
+
+
+**Solução - Parte III **
+A solução adotada foi implementar uma recursão. O caso base é quando o
+comprimento é zero. A única cadeia de tamanho zero é a cadeia vazia. Nos demais
+casos, gera-se todas as cadeias de tamanho $n-1$ e em seguida gera-se duas
+semi-duplicadas, uma com 0 acrescentando à frente de cada cadeia, e outra com 1
+acrescentado à frente de cada cadeia. O resultado é a concatenação das duas
+semi-duplicatas.
+
+**Execução**
+Para executar a função no prompt do Clojure execute, por exemplo:
+```
+#> lein repl
+(ns clirc.core (:require [clirc.probl1.probl1 :as cmp]))
+(def n 3) ;; 3 é o numero de bits que serão comparados
+(cmp/cmp-n-bits n) 
+(println (aon-eval (cmp/cmp-n-bits n) [0 0 1, 0 0 0])))
+```
--- src/clirc/probl1/probl1.clj
+++ src/clirc/probl1/probl1.clj
@@ -1,12 +1,34 @@
 (ns clirc.probl1.probl1)
 
+" Recebe um número N e retorna a string 'tempN' "
 (defn create-temp [n] (symbol (str "temp" n)))
 
+" Constantes "
+(def aon_constant 5)
+(def nand_constant 6)
 
-(defn cmp-n-bits-helper
+" 
+ Função recursiva auxiliar  responsável por montar a estrutura da lista com o padrão de crescimento encontrado (da quantidade de operadores necessários)
+ ao aumentar a quantidade de bits a serem comparados.
+
+`Atual` : A 'base', quantos operadores já foram contados. A primeira chamada deverá ter o número 2 nesse parâmetro, pois ao comparar
+ 2 números de um bit, são necessários apenas 2 operadores. O padrão é observado a partir da comparação de 2+ bits
+
+ Total : total de bits a serem comparados
+
+ Retorno: lista no formato CLIRC. E.g.: [(set! temp1 (not (:in 5)))
+                                             (set! temp2 (and (:in 2) temp1))
+                                             (set! temp3 (not (:in 4))) ...]
+"
+(defn cmp-n-bits-helper-aon
   [atual total]
   (loop [_atual atual _total total _lista []]
-    (let [shift (+ (* (- _atual 2) 5) 2)
+  ;; Shift é utilizado para nomeação de variáveis temporárias
+;; Digamos que atual seja 2 (ou seja, contabilizamos as duas operações da comparação entre 1 bit e outro)
+;; As variáveis temp1 e temp2 estão criadas. Qual será a proxima?
+;; O resultado do shift seria 2, então na hora de nomear, adicionamos progressivamente os números necessários
+
+    (let [shift (+ (* (- _atual 2) aon_constant) 2)
           indiceA (- _total _atual)
           indiceB (- (* _total 2) _atual)
           result (into [] (concat _lista
@@ -15,11 +37,21 @@
                                     (set! ~(create-temp (+ shift 3)) (~(symbol "or") ~(list :in indiceA) ~(create-temp (+ shift 1))))
                                     (set! ~(create-temp (+ shift 4)) (~(symbol "and") ~(create-temp (+ shift 0)) ~(create-temp (+ shift 3))))
                                     (set! ~(create-temp (+ shift 5)) (~(symbol "or") ~(create-temp (+ shift 4)) ~(create-temp (+ shift 2))))]))]
+      ;; Verificação de parada/continuação
       (cond (= _atual _total)
             (into [] (concat result `[(set! ~(list :out 0) (~(symbol "and") ~(create-temp (+ shift 5)) ~(create-temp (+ shift 5))))]))
             :else (recur (+ _atual 1) _total result)))))
 
-(defn cmp-n-bits
+"
+Comparador de N bits com funções AON.
+
+n : Quantidade de bits dos números a serem comparados
+
+Retorno :  lista no formato CLIRC. E.g.: [(set! temp1 (not (:in 5)))
+                                             (set! temp2 (and (:in 2) temp1))
+                                             (set! temp3 (not (:in 4))) ...]
+"
+(defn cmp-n-bits-aon
   [n]
   (cond
     (= n 1)
@@ -28,6 +60,48 @@
       (set! (:out 0) (and temp2 temp2))]
     :else
     (into [] (concat `[;; Comparador do bit menos significativo
+                       ;; in contém as posições do array de input, se o input é [0 0 0 1 1 1], (in: 3) seria input[3] = 1
                        (set! ~(symbol "temp1") (~(symbol "not") ~(list :in (- (* n 2) 1))))
                        (set! ~(symbol "temp2") (~(symbol "and") ~(list :in (- n 1)) ~(symbol "temp1")))]
-                     (cmp-n-bits-helper 2 n)))))
+                     (cmp-n-bits-helper-aon 2 n)))))
+
+
+
+; In Progress
+; 010 010
+; (defn cmp-n-bits-helper-nand
+;   3       4
+;   [atual total]
+;   (loop [_atual atual _total total _lista []]
+;     shift = 3
+;     (let [shift (+ (* (- _atual 3) nand_constant) 3)
+;           1
+;           indiceA (- _total _atual)
+;           3
+;           indiceB (- (* _total 2) _atual)
+;           result (into [] (concat _lista
+                                 ; `[(set! ~(create-temp (+ shift 1)) (~(symbol "nand") ~(list :in indiceB)))
+                                 ;   (set! ~(create-temp (+ shift 2)) (~(symbol "nand") ~(list :in indiceA) ~(create-temp (+ shift 1))))
+                                 ;   (set! ~(create-temp (+ shift 3)) (~(symbol "nand") ~(list :in indiceA) ~(create-temp (+ shift 1))))
+                                 ;   (set! ~(create-temp (+ shift 4)) (~(symbol "nand") ~(create-temp (+ shift 0)) ~(create-temp (+ shift 3))))
+                                 ;   (set! ~(create-temp (+ shift 5)) (~(symbol "nand") ~(create-temp (+ shift 4)) ~(create-temp (+ shift 2))))
+                                 ;   (set! ~(create-temp (+ shift 6)) (~(symbol "nand") ~(create-temp (+ shift 4)) ~(create-temp (+ shift 2))))]))]
+      ;; Verificação de parada/continuação
+      ;(cond (= _atual _total)
+            ;(into [] (concat result `[(set! ~(list :out 0) (~(symbol "nand") ~(create-temp (+ shift 5)) ~(create-temp (+ shift 5))))]))
+           ; :else (recur (+ _atual 1) _total result)))))
+
+;; In progress
+;(defn cmp-n-bits-nand
+;  [n]
+;  (= n 1)
+;    '[(set! temp1 (nand (:in 0) (:in 1)))
+;      (set! temp2 (nand (:in 0) temp1)) 
+;      (set! (:out 0) (nand temp2 temp2))]
+;    :else
+;    (into [] (concat `[;; Comparador do bit menos significativo
+;                       ;; in contém as posições do array de input, se o input é [0 0 0 1 1 1], (in: 3) seria input[3] = 1
+;                       (set! ~(symbol "temp1") (~(symbol "not") ~(list :in (- (* n 2) 1))))
+;                       (set! ~(symbol "temp2") (~(symbol "and") ~(list :in (- n 1)) ~(symbol "temp1")))]
+;                     (cmp-n-bits-helper-nand 3 (+ n 1))))
+;  )
